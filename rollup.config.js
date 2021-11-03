d50ad883e29555981b73733bdf93b900c4f3464b
jimmy yen <cy.yen1988@gmail.com>
2021-11-03 23:53:44 +0800

    引入包

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import merge from 'deepmerge';
-import { createBasicConfig } from '@open-wc/building-rollup';
+import merge from "deepmerge";
+import { createBasicConfig } from "@open-wc/building-rollup";
 
 const baseConfig = createBasicConfig();
 
 export default merge(baseConfig, {
-  input: './src/index.ts',
+  input: "./src/index.ts",
   output: [
     {
       format: "cjs",
-      file: "dist/lottery-logic.cjs.js"
+      file: "dist/lottery-logic.cjs.js",
     },
     {
       format: "es",
-      file: "dist/lottery-logic.esm.js"
-    }
-  ]
+      file: "dist/lottery-logic.esm.js",
+    },
+  ],
 });
