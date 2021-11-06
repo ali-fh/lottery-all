1f9411f0dd45ec8ca63e7414ebc888ae0e0c18d7
jimmy yen <cy.yen1988@gmail.com>
2021-11-06 18:32:29 +0800

    commit

--- src/rules/ruleSSC.ts
+++ src/rules/ruleSSC.ts
@@ -1,22 +1,175 @@
-import BetOptionsGeneratorK3 from '../optionsGenerators/BetOptionsGeneratorK3'
+import BetOptionsGenerator from '../optionsGenerators/BetOptionsGenerator'
 
-const OptionsGeneratorK3 = new BetOptionsGeneratorK3()
+const OptionsGenerator = new BetOptionsGenerator()
+
+const DIGIT_ARRAY = ['万位', '千位', '百位', '十位', '个位']
 
 export default {
-  // 和值
-  'k3-k3-hezhi': { betOptions: OptionsGeneratorK3.generatBetOptions(OptionsGeneratorK3.generatButtonNumbers(3, 18, true), true) },
-  // 大小单双
-  'k3-k3-daxiaodanshuang': { betOptions: OptionsGeneratorK3.generatBetOptions(['大', '小', '单', '双']) },
-  // 三同号
-  'k3-k3-santonghao': { betOptions: OptionsGeneratorK3.generatBetOptions(['666', '555', '444', '333', '222', '111']) },
-  // 三不同号
-  'k3-k3-sanbutonghao': { betOptions: [] },
-  // 三连号
-  'k3-k3-sanlianhao': { betOptions: OptionsGeneratorK3.generatBetOptions(['123', '234', '345', '456']) },
-  // 二不同号
-  'k3-k3-erbutonghao': { betOptions: [] },
-  // 二同号
-  'k3-k3-ertonghao': { betOptions: OptionsGeneratorK3.generatBetOptions(OptionsGeneratorK3.generatButtonNumbersErTongHao()) },
-  // 单挑一骰
-  'k3-k3-dantiaoyishai': { betOptions: OptionsGeneratorK3.generatBetOptions(['6', '5', '4', '3', '2', '1']) }
+  'yixing-dingweidan-fushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY)
+  },
+  'wuxing-zhixuan-fushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY)
+  },
+  'sixing-zhixuan-fushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY.slice(1))
+  },
+  'qiansan-zhixuan-fushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY.slice(2))
+  },
+  'zhongsan-zhixuan-fushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY.slice(1, 4))
+  },
+  'housan-zhixuan-fushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY.slice(2))
+  },
+  'erxing-zhixuan-houerfushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY.slice(3))
+  },
+  'erxing-zhixuan-qianerfushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY.slice(0, 2))
+  },
+  'renxuan-renxuan2-zhixuanfushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY)
+  },
+  'renxuan-renxuan3-zhixuanfushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY)
+  },
+  'renxuan-renxuan4-zhixuanfushi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 9, true), true, DIGIT_ARRAY)
+  },
+  'renxuan-renxuan2-zhixuandanshi': {},
+  'renxuan-renxuan3-zhixuandanshi': {},
+  'renxuan-renxuan4-zhixuandanshi': {},
+  'renxuan-renxuan2-zuxuandanshi': {},
+  'renxuan-renxuan3-zusandanshi': {},
+  'renxuan-renxuan3-zuliudanshi': {},
+  'renxuan-renxuan3-hunhezuxuan': {},
+  'renxuan-renxuan2-zhixuanhezhi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 18, true), true)
+  },
+  'renxuan-renxuan3-zhixuanhezhi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 27, true), true)
+  },
+  'renxuan-renxuan2-zuxuanhezhi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 17, true), true)
+  },
+  'renxuan-renxuan3-zuxuanhezhi': {
+    betOptions: OptionsGenerator.generatBetOptions(OptionsGenerator.generatButtonNumbers(0, 26, true), true)
+  },
+  'renxuan-renxuan2-zuxuanfushi': {},
+  'renxuan-renxuan3-zusanfushi': {},
+  'renxuan-renxuan4-zuxuan24': {},
+  'renxuan-renxuan4-zuxuan12': {},
+  'renxuan-renxuan4-zuxuan6': {},
+  'renxuan-renxuan4-zuxuan4': {},
+  'renxuan-renxuan3-zuliufushi': {},
+  'hezhi-wuxing-hezhi': {},
+  'wuxing-zhixuan-danshi': {},
+  'sixing-zhixuan-danshi': {},
+  'qiansan-zhixuan-danshi': {},
+  'housan-zhixuan-danshi': {},
+  'erxing-zhixuan-houerdanshi': {},
+  'erxing-zhixuan-qianerdanshi': {},
+  'qiansan-zuxuan-zusandanshi': {},
+  'zhongsan-zuxuan-zusandanshi': {},
+  'housan-zuxuan-zusandanshi': {},
+  'qiansan-zuxuan-zuliudanshi': {},
+  'zhongsan-zuxuan-zuliudanshi': {},
+  'housan-zuxuan-zuliudanshi': {},
+  'erxing-zuxuan-houerdanshi': {},
+  'erxing-zuxuan-qianerdanshi': {},
+  'qiansan-zuxuan-hunhezuxuan': {},
+  'zhongsan-zuxuan-hunhezuxuan': {},
+  'housan-zuxuan-hunhezuxuan': {},
+  'zhongsan-zhixuan-danshi': {},
+  'wuxing-zhixuan-zuhe': {},
+  'sixing-zhixuan-zuhe': {},
+  'qiansan-zhixuan-zuhe': {},
+  'zhongsan-zhixuan-zuhe': {},
+  'housan-zhixuan-zuhe': {},
+  'wuxing-zuxuan-zuxuan120': {},
+  'sixing-zuxuan-zuxuan24': {},
+  'erxing-zuxuan-houerfushi': {},
+  'erxing-zuxuan-qianerfushi': {},
+  'budingwei-sanxingbudingwei-housanermabudingwei': {},
+  'budingwei-sanxingbudingwei-qiansanermabudingwei': {},
+  'budingwei-sanxingbudingwei-zhongsanermabudingwei': {},
+  'budingwei-sixingbudingwei-sixingermabudingwei': {},
+  'budingwei-wuxingbudingwei-wuxingermabudingwei': {},
+  'budingwei-wuxingbudingwei-wuxingsanmabudingwei': {},
+  'wuxing-zuxuan-zuxuan60': {},
+  'sixing-zuxuan-zuxuan12': {},
+  'wuxing-zuxuan-zuxuan30': {},
+  'wuxing-zuxuan-zuxuan20': {},
+  'sixing-zuxuan-zuxuan4': {},
+  'wuxing-zuxuan-zuxuan10': {},
+  'wuxing-zuxuan-zuxuan5': {},
+  'sixing-zuxuan-zuxuan6': {},
+  'qiansan-zuxuan-zusan': {},
+  'zhongsan-zuxuan-zusan': {},
+  'housan-zuxuan-zusan': {},
+  'qiansan-zuxuan-zuliu': {},
+  'zhongsan-zuxuan-zuliu': {},
+  'housan-zuxuan-zuliu': {},
+  'qiansan-zhixuan-hezhi': {},
+  'zhongsan-zhixuan-hezhi': {},
+  'housan-zhixuan-hezhi': {},
+  'erxing-zhixuan-houerhezhi': {},
+  'erxing-zhixuan-qianerhezhi': {},
+  'qiansan-zuxuan-hezhi': {},
+  'zhongsan-zuxuan-hezhi': {},
+  'housan-zuxuan-hezhi': {},
+  'erxing-zuxuan-houerhezhi': {},
+  'erxing-zuxuan-qianerhezhi': {},
+  'qiansan-zhixuan-kuadu': {},
+  'zhongsan-zhixuan-kuadu': {},
+  'housan-zhixuan-kuadu': {},
+  'erxing-zhixuan-houerkuadu': {},
+  'erxing-zhixuan-qianerkuadu': {},
+  'quwei-teshu-yifanfengshun': {},
+  'quwei-teshu-haoshichengshuang': {},
+  'quwei-teshu-sanxingbaoxi': {},
+  'quwei-teshu-sijifacai': {},
+  'budingwei-sanxingbudingwei-housanyimabudingwei': {},
+  'budingwei-sanxingbudingwei-qiansanyimabudingwei': {},
+  'budingwei-sanxingbudingwei-zhongsanyimabudingwei': {},
+  'budingwei-sixingbudingwei-sixingyimabudingwei': {},
+  'qiansan-zuxuan-baodan': {},
+  'zhongsan-zuxuan-baodan': {},
+  'housan-zuxuan-baodan': {},
+  'erxing-zuxuan-houerbaodan': {},
+  'erxing-zuxuan-qianerbaodan': {},
+  'qiansan-qita-hezhiweishu': {},
+  'zhongsan-qita-hezhiweishu': {},
+  'housan-qita-hezhiweishu': {},
+  'qiansan-qita-teshuhaoma': {},
+  'zhongsan-qita-teshuhaoma': {},
+  'housan-qita-teshuhaoma': {},
+  'hezhi-wuxing-bsde': {},
+  'daxiaodanshuang-daxiaodanshuang-houerdaxiaodanshuang': {},
+  'daxiaodanshuang-daxiaodanshuang-housandaxiaodanshuang': {},
+  'daxiaodanshuang-daxiaodanshuang-qianerdaxiaodanshuang': {},
+  'daxiaodanshuang-daxiaodanshuang-qiansandaxiaodanshuang': {},
+  'daxiaodanshuang-daxiaodanshuang-zhongsandaxiaodanshuang': {},
+  'quwei-quwei-wumaquweisanxing': {},
+  'quwei-quwei-simaquweisanxing': {},
+  'quwei-quwei-housanquweierxing': {},
+  'quwei-quwei-qiansanquweierxing': {},
+  'quwei-quwei-zhongsanquweierxing': {},
+  'quwei-qujian-wumaqujiansanxing': {},
+  'quwei-qujian-simaqujiansanxing': {},
+  'quwei-qujian-housanqujianerxing': {},
+  'quwei-qujian-qiansanqujianerxing': {},
+  'quwei-qujian-zhongsanqujianerxing': {},
+  'longhu-longhuhe-wanqian': {},
+  'longhu-longhuhe-wanbai': {},
+  'longhu-longhuhe-wanshi': {},
+  'longhu-longhuhe-wange': {},
+  'longhu-longhuhe-qianbai': {},
+  'longhu-longhuhe-qianshi': {},
+  'longhu-longhuhe-qiange': {},
+  'longhu-longhuhe-baishi': {},
+  'longhu-longhuhe-baige': {},
+  'longhu-longhuhe-shige': {}
 }
