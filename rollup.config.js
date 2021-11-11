3b03d97da476561b556dc9b2a6391bbb28f47089
jimmy yen <cy.yen1988@gmail.com>
2021-11-11 12:51:58 +0800

    build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,39 @@
-import merge from "deepmerge";
-import { createBasicConfig } from "@open-wc/building-rollup";
+import merge from 'deepmerge'
+import { createBasicConfig } from '@open-wc/building-rollup'
+import typescript from 'rollup-plugin-typescript'
+import commonjs from 'rollup-plugin-commonjs'
 
-const baseConfig = createBasicConfig();
+const baseConfig = createBasicConfig()
 
 export default merge(baseConfig, {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: [
     {
-      format: "cjs",
-      file: "dist/lottery-logic.cjs.js",
+      format: 'cjs',
+      file: 'dist/lottery-logic.cjs.js'
     },
     {
-      format: "es",
-      file: "dist/lottery-logic.esm.js",
-    },
+      format: 'es',
+      file: 'dist/lottery-logic.esm.js'
+    }
   ],
-});
+  plugins: [
+    typescript({
+      exclude: 'node_modules/**',
+      typescript: require('typescript')
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'node_modules/lodash/lodash.js': ['cloneDeep']
+      }
+    })
+  ],
+  onwarn: function (warning) {
+    if (warning.code === 'THIS_IS_UNDEFINED') {
+      return
+    }
+
+    console.warn(warning.message)
+  }
+})
