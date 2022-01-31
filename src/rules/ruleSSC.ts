cb6ea7f6495f669684bbde8aa22313fc5b868c8e
jimmy yen <cy.yen1988@gmail.com>
2022-01-31 13:36:40 +0800

    Bug #6628:【2hz迁移】【h5游戏】时时彩，任选--任选二--组选单式，按照规则输入后，不能下注；还需检查修改后是否能成功下注

--- src/rules/ruleSSC.ts
+++ src/rules/ruleSSC.ts
@@ -291,7 +291,7 @@ export default {
     noOrder: true,
     position: [false, false, false, true, true],
     positionLimit: 2,
-    positionBetCount: (num: number, position: Array<boolean>) => positionBet(num, position, 3),
+    positionBetCount: (num: number, position: Array<boolean>) => positionBet(num, position, 2),
     getProfit: function (data: ProfitParams) {
       return ProfitCalculator.profitWrapper(this.position, [], data, [{}, {}, { 1: 3 }, { 1: 6 }, { 1: 10 }], 2)
     },
